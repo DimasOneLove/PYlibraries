630b62100c3c91dff4f48040c1b3f3dcd77ec378
DimasOneLove <puchkov_dmitrij_05@mail.ru>
2025-02-09 23:49:29 +0300

    Done tasks 7-9, continuation summary

--- Lesson1 (NumPy)/hw1.py
+++ Lesson1 (NumPy)/hw1.py
@@ -47,3 +47,43 @@ print(np.random.normal(0, 1, 5))
 
 ## 6. Создание массива с 5 случайными целыми числами [0, 10)
 print(np.random.randint(0, 10, 5))
+
+
+### Срезы
+
+## 7. Написать кода для создания срезов массива 3 на 4
+# - первые две строки и три столбца
+# - первые три строки и второй столбец
+# - все строки и столбцы в обратном порядке
+# - второй столбец
+# - третья строка
+np.random.seed(seed = 0)
+arr = np.random.randint(10, size = (3,4))
+print(arr)
+print(arr[:2, :3])
+print(arr[:3, 1:2])
+print(arr[::-1, ::-1])
+print(arr[:, 1:2])
+print(arr[2:3, :])
+
+## 8. Срез-копия
+# с помощью .copy()
+arr1 = np.random.randint(10, size = 8)
+print(arr1)
+arr2 = arr1[2:5].copy()
+print(arr2)
+print(arr1)
+# с помощью np.array()
+arr3 = np.array(arr2[::-1])
+print(arr3)
+print(arr2)
+
+## 9. Использование newaxis для получения вектора-столбца и вектора-строки из массива
+print(arr1)
+vector_column = arr1[:, np.newaxis]
+print(vector_column)
+
+vector_row = arr1[np.newaxis, :]
+vector_row1 = arr1[None, :] # newaxis == None
+print(vector_row)
+print(vector_row1)
--- Lesson1 (NumPy)/lesson1.py
+++ Lesson1 (NumPy)/lesson1.py
@@ -68,3 +68,73 @@ print(np.eye(4, dtype = int))
 
 ### Массивы
 
+np.random.seed(1)
+
+x1 = np.random.randint(10, size = 3)
+x2 = np.random.randint(10, size = (3, 2))
+x3 = np.random.randint(10, size = (3, 2, 1))
+
+print(x1)
+print(x2)
+print(x3)
+
+# атрибуты массива
+print(x1.ndim, x1.shape, x1.size) # чило разрерностей, размер каждой размерности, общий размер массива
+print(x2.ndim, x2.shape, x2.size)
+print(x3.ndim, x3.shape, x3.size)
+
+#доступ к элементам массива
+a = np.array([1,2,3,4,5])
+print(a[0])
+print(a[-2])
+a[1] = 20
+print(a)
+
+a = np.array([[1,2], [3,4]])
+print(a)
+print(a[0,0])
+print(a[-1, -1])
+a[1, 0] = 100
+print(a)
+
+a = np.array([1,2,3,4,5])
+b = np.array([1.0,2,3,4,5])
+
+print(a)
+print(b)
+
+a[0] = 10
+print(a)
+
+a[0] = 10.123
+print(a) # зафиксирован тип массива, изменение типа не происходит
+
+
+### Срезы [start: finish: step], по умолчанию [1: shape: 1]
+
+a = np.array([1,2,3,4,5,6])
+print([x for x in a.shape], a.size)
+print(a[:3])
+print(a[3:])
+print(a[1:5])
+print(a[1:-1])
+print(a[1::2])
+print(a[::-1])
+
+# --- см. дз № 7 ---
+
+b = a[:3]
+print(b)
+
+b[0] = 100
+print(a) # изменилось а
+# срез - не копия, а ссылка
+
+# --- см. дз № 8 ---
+
+a = np.arange(1, 13)
+print(a)
+print(a.reshape(2,6))
+print(a.reshape(3,4))
+
+# --- см. дз № 9 ---
