0f31e35e413dd0c4e3b9a7c3313179779bebbbd2
DimasOneLove <puchkov_dmitrij_05@mail.ru>
2025-02-26 00:24:17 +0300

    Finish summit

--- Lesson3 (Pandas)/lesson3.py
+++ Lesson3 (Pandas)/lesson3.py
@@ -119,6 +119,7 @@ print(indA.intersection(indB))
 ### Выборка данных:
 
 ## 1) Из Series
+## как словарь
 data = pd.Series([0.25, 0.5, 0.75, 1.0], index = ['a', 'b', 'c', 'd'])
 print('a' in data)
 print('z' in data)
@@ -138,3 +139,189 @@ print(data[1]) # индексы строчные -> 1 это индеккс дл
 
 data = pd.Series([0.25, 0.5, 0.75, 1.0], index = [1, 3, 10, 15])
 print(data[1]) # выведется по индексу (из массива индексов), а не позиционный индекс стандартного массива
+
+# атрибуты-индексаторы
+print(data.loc[1]) # из массива индексов
+print(data.iloc[1]) # номер элемента как в обычном списке
+
+
+## 2) Из DataFrame
+## как словарь
+pop = pd.Series(
+    {
+        'city_1': 1001,
+        'city_2': 1002,
+        'city_3': 1003,
+        'city_4': 1004,
+        'city_5': 1005,
+    })
+
+area = pd.Series(
+    {
+        'city_1': 9991,
+        'city_2': 9992,
+        'city_3': 9993,
+        'city_4': 9994,
+        'city_5': 9995,
+    })
+
+data = pd.DataFrame({'area1':area, 'pop1': pop, 'pop': pop})
+print(data)
+print(data['area1'])
+print(data.area1)
+
+print(data.pop1 is data['pop1'])
+print(data.pop is data['pop']) # конфликт названия с методом
+
+data['new'] = data['area1']
+print(data)
+data['new1'] = data['area1'] / data['pop1']
+print(data)
+
+## как двумерный NumPy-массив
+data = pd.DataFrame({'area1':area, 'pop1': pop})
+print(data)
+print(data.values)
+print(data.T)
+print(data['area1'])
+print(data.values[0])
+print(data.values[0:3])
+
+# атрибуты-индексаторы
+data = pd.DataFrame({'area1':area, 'pop1': pop, 'pop': pop})
+print(data)
+print(data.iloc[:3, 1:2])
+print(data.loc[:'city_4', 'pop1':'pop']) # включая обе границы
+print(data.loc[data['pop'] > 1002, ['area1','pop']])
+data.iloc[0,2] = 99999
+print(data)
+
+
+# про универсальные функции
+rng = np.random.default_rng()
+s = pd.Series(rng.integers(0,10,4))
+print(s)
+print(np.exp(s))
+
+
+# если часть элементов (ключей) не совпадает
+pop = pd.Series(
+    {
+        'city_1': 1001,
+        'city_2': 1002,
+        'city_3': 1003,
+        'city_41': 1004,
+        'city_51': 1005,
+    })
+
+area = pd.Series(
+    {
+        'city_1': 9991,
+        'city_2': 9992,
+        'city_3': 9993,
+        'city_42': 9994,
+        'city_52': 9995,
+    })
+data = pd.DataFrame({'area1':area, 'pop1': pop})
+print(data) # объединяется и добавляет NaN туда, где остутствует значение
+
+# --- см. дз №3 ---
+
+dfA = pd.DataFrame(rng.integers(0, 10, (2,2)), columns = ['a', 'b'])
+dfB = pd.DataFrame(rng.integers(0, 10, (3,3)), columns = ['a', 'b', 'c'])
+print(dfA)
+print(dfB)
+print(dfA + dfB) # объединение DataFrame'ов
+
+
+rng = np.random.default_rng(1)
+A = rng.integers(0, 10, (3,4))
+print(A)
+print(A[0])
+print(A - A[0])
+
+df = pd.DataFrame(A, columns=['a', 'b', 'c', 'd'])
+print(df)
+print(df.iloc[0])
+print(df - df.iloc[0])
+print(df.iloc[0, ::2])
+print(df - df.iloc[0, ::2])
+
+# --- см. дз №4 ---
+
+
+# NA-значения: NaN, null, -99999
+
+# Pandas. Два способа хранения отсутствующих значений
+# индикаторы Nan, None
+# null
+
+# None - объект(накладные расходы). Не работает с sum, min
+
+# NumPy
+val1 = np.array([1,2,3])
+print(val1.sum())
+
+val2 = np.array([1,None,3])
+# print(val2.sum()) - не будет работать
+
+val1 = np.array([1, np.nan, 2, 3])
+print(val1)
+print(val1.sum())
+print(np.nansum(val1))
+
+# Pandas
+x = pd.Series(range(10), dtype=int)
+print(x)
+x[0] = None
+x[1] = np.nan
+print(x)
+
+x1 = pd.Series(['a', 'b', 'c'])
+print(x1)
+x1[0] = None
+x1[1] = np.nan
+print(x1)
+# для целых чисел None и np.nan ведут себя одинаково (выдают NaN), а в случае объектов (строк, boolean) по-разному
+
+# <NA> - Pandas
+x2 = pd.Series([1,2,3, np.nan, None, pd.NA])
+print(x2)
+
+x3 = pd.Series([1,2,3, np.nan, None, pd.NA], dtype='Int32')
+print(x3) #если приводим к одному типу то Nan тоже приводятся к одному типу <NA>
+
+print(x3.isnull())
+print(x3[x3.isnull()])
+print(x3[x3.notnull()])
+print(x3.dropna())
+
+
+df = pd.DataFrame(
+    [
+        [1,2,3,np.nan,None,pd.NA],
+        [1,2,3,4,5,6],
+        [1,np.nan,3,4,np.nan,6],
+    ]
+)
+print(df)
+print(df.dropna())
+print('-----')
+print(df.dropna(axis=0))
+print('-----')
+print(df.dropna(axis=1))
+
+# how:
+# - all -все значения NA
+# - any -хотя бы одно значение 
+# - thresh = x, остается если присутствует МИНИМУМ х НЕПУСТЫХ значений
+df = pd.DataFrame(
+    [
+        [1,2,3,np.nan,None,pd.NA],
+        [1,2,3,None,5,6],
+        [1,np.nan,3,None,np.nan,6],
+    ]
+)
+print(df.dropna(axis=1, how='all'))
+print(df.dropna(axis=1, how='any'))
+print(df.dropna(axis=1, thresh=2))
